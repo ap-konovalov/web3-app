8c6aeae9a8db1209d02b01d3327fad80411a0a3c
akonovalov <work.apkonovalov@gmail.com>
2021-03-19 11:37:29 +0300

    when auth by WavesSigner then show user address

--- src/components/SignerBlock.js
+++ src/components/SignerBlock.js
@@ -7,18 +7,25 @@ const signer = new Signer({
 });
 signer.setProvider(new ProviderWeb('https://testnet.waves.exchange/signer/'))
 
+let address = 'not authorized'
+
 async function auth() {
     try {
         const user = await signer.login();
+        address = user.address;
     } catch (e) {
         console.error('login rejected')
+    } finally {
+        document.querySelector(".address").innerHTML = "Your address is: " + address
     }
 }
 
 export function SignerBlock() {
     return (
         <section className="signer_buttons">
+            <br/>
             <input className="btn btn-primary" type="submit" value="Auth with WavesSigner" onClick={auth}/>
+            <p class="address"></p>
         </section>
     );
 }
